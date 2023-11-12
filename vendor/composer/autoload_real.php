645329851dfa2071651bcedf5735d9528dc4c7e7
Pahom21 <elviswahome.ew@gmail.com>
2023-11-12 20:14:59 +0300

    Final commit under logout

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit05f20aa45da695ec87e1ce112bafbd50
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit05f20aa45da695ec87e1ce112bafbd50::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
